f02c0cfd596ce53b6fd75b8d72e89ce722c1bccc
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-06-13 16:38:58 +0200

    search function done in the blog view as well as configured the search form in the base.html

--- app/views.py
+++ app/views.py
@@ -3,15 +3,30 @@ from .models import Blog
 from .forms import BlogForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def home(request):
+
+    # search functionality
+    search_query = request.GET.get("q", "")
     blogs = Blog.objects.all().order_by("-created_at")
+    if search_query:
+        blogs = blogs.filter(
+            Q(title__icontains=search_query) |
+            Q(content__icontains=search_query) |
+            Q(features__name__icontains=search_query) |
+            Q(tech_stack__name__icontains=search_query)
+        ).distinct()
+    if not blogs:
+        messages.warning(request, "No results found for your search query.")
+    # pagination
     paginator = Paginator(blogs, 6)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     context = {
         "page_obj": page_obj,
+        "search_query": search_query,
     }
     return render(request, "index.html", context)
 
